28a5bb8519eb43f5f3aafc326e08412e8fb2f131
ibtihelgombra <betigomgom@gmail.com>
2024-09-07 00:42:42 +0000

    Initialize project using Create React App

---
+++ src/product.js
@@ -0,0 +1,10 @@
+// src/product.js
+const product = {
+    name: "Awesome Product",
+    price: "$29.99",
+    description: "This is an awesome product that you'll love.",
+    image: "https://via.placeholder.com/150"
+  };
+  
+  export default product;
+  
